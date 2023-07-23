2f758b60f9fde04346f7c8ef44461cf3b68c618b
Yuti Thakkar <thakkaryuti@gmail.com>
2023-07-23 16:18:07 -0400

    adding test results

---
+++ index.js
@@ -0,0 +1,19 @@
+// Define the Scooter constructor
+function Scooter(year, color, model) {
+  this.year = year;
+  this.color = color;
+  this.model = model;
+}
+
+// Define the Driver constructor
+function Driver(name, age, experience) {
+  this.name = name;
+  this.age = age;
+  this.experience = experience;
+}
+
+// Define the PickupLocation constructor
+function PickupLocation(address, city) {
+  this.address = address;
+  this.city = city;
+}
